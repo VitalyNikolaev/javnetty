f7ecda3eb86de5788bc89d23c5b30c0623bc44a1
Vitaly Nikolaev <vit3011@rambler.ru>
2016-09-12 01:15:13 +0300

    inital commit

---
+++ src/HTTP/HTTP.java
@@ -0,0 +1,7 @@
+package HTTP;
+import io.netty.buffer.ByteBuf;
+/**
+ * Created by nikolaev on 11.09.16.
+ */
+public class HTTP {
+}
---
+++ src/Main/Main.java
@@ -0,0 +1,34 @@
+package Main;
+import io.netty.buffer.ByteBuf;
+
+public class Main {
+    private static int threadsCount = Runtime.getRuntime().availableProcessors();
+    private static int port = 80;
+    private static String rootDir;
+
+    public static void main(String[] args) {
+        final setUpFromCommandLine settings = new setUpFromCommandLine();
+        settings.parseCommands(args);
+        System.out.println("args = [" + threadsCount + "]");
+    }
+
+    private static class setUpFromCommandLine {
+        void parseCommands(String[] args) {
+            for (int i = 0; i < args.length; i += 2) {
+                switch (args[i]) {
+                    case "-r":
+                        rootDir = (args[i + 1]);
+                        break;
+                    case "-c":
+                        threadsCount = (Integer.parseInt(args[i + 1]));
+                        break;
+                    case "-p":
+                        port = (Integer.parseInt(args[i + 1]));
+                        break;
+
+                }
+            }
+
+        }
+    }
+}
